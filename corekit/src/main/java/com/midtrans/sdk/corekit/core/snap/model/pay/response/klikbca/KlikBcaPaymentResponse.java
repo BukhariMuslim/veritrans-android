96d2e22e175828db2d99c173a67a5f29e30bf1ed
suryagsaputra <suryagsaputra@gmail.com>
2018-11-27 18:01:20 +0700

    remove unused

--- corekit/src/main/java/com/midtrans/sdk/corekit/core/snap/model/pay/response/klikbca/KlikBcaPaymentResponse.java
+++ corekit/src/main/java/com/midtrans/sdk/corekit/core/snap/model/pay/response/klikbca/KlikBcaPaymentResponse.java
@@ -7,9 +7,4 @@ import com.midtrans.sdk.corekit.core.snap.model.pay.response.BasePaymentResponse
 public class KlikBcaPaymentResponse extends BasePaymentResponse {
     @SerializedName("approval_code")
     private String approvalCode;
-
-
-    @SerializedName("bcak")
-    private String approvalCode;
-
 }
