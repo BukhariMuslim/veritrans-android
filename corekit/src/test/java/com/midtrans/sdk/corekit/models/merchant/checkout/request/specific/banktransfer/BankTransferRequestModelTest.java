14cf4fc4ad6201597cb03fc974c7697cd614fe82
Surya <suryagsaputra@gmail.com>
2018-11-27 16:31:36 +0700

    update bank transfer request model unit test

--- corekit/src/test/java/com/midtrans/sdk/corekit/models/merchant/checkout/request/specific/banktransfer/BankTransferRequestModelTest.java
+++ corekit/src/test/java/com/midtrans/sdk/corekit/models/merchant/checkout/request/specific/banktransfer/BankTransferRequestModelTest.java
@@ -1,9 +1,14 @@
 package com.midtrans.sdk.corekit.models.merchant.checkout.request.specific.banktransfer;
 
 import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.specific.banktransfer.BankTransferRequestModel;
+import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.specific.banktransfer.BcaBankFreeText;
+import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.specific.banktransfer.BcaBankFreeTextLanguage;
+import com.midtrans.sdk.corekit.core.merchant.model.checkout.request.specific.banktransfer.BcaBankTransferRequestModel;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -15,7 +20,13 @@ public class BankTransferRequestModelTest {
 
     @Before
     public void test_setup() {
-        this.bankTransferRequestModel = new BankTransferRequestModel();
+        this.bankTransferRequestModel = new BcaBankTransferRequestModel(
+                "",
+                new BcaBankFreeText(
+                        new ArrayList<BcaBankFreeTextLanguage>(),
+                        new ArrayList<BcaBankFreeTextLanguage>()),
+                ""
+        );
         this.exampleTextPositive = "exampleTextPositive";
         this.exampleTextNegative = "exampleTextNegative";
     }
